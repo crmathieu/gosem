42107d1e33a495c59c911d031fbffadcbeb7a7c8
crmathieu <charles@gawkbox.com>
2019-08-08 16:46:30 -0700

    added sync package

--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -8,24 +8,41 @@ import (
 const MAXTHREADS = 100
 
 type Sync struct {
-	syncsem *sem.Sem
+	syncmutex *sem.Mutex
+	csync   chan int
+	cnt 	int
+	in_use 	bool  
 }
 
 func NewSync(maxthreads int) *Sync {
 	if maxthreads <= 0 || maxthreads > MAXTHREADS {
 		maxthreads = MAXTHREADS
 	}
-	return &Sync{sem.Createsem("sync-sem", maxthreads, 0)}
+	return &Sync{sem.Createmutex("sync-mutex"), make(chan int, 1), 0, false}
 }
 
-func (s *Sync)Add(n int) {
-	for i:=0; i<n; i++ {
-		s.syncsem.Wait()
+func (s *Sync)Add() {
+	s.syncmutex.Enter()
+	if s.in_use == false {
+		s.in_use = true
 	}
+	s.cnt++
+	s.syncmutex.Leave()
 }
 
-func (s *Sync)Done(n int) {
-	for i:=0; i<n; i++ {
-		s.syncsem.Signal()
+func (s *Sync)Done() {
+	s.syncmutex.Enter()
+	if s.in_use == false || s.cnt < 1 {
+		fmt.Println("Invalid Done op")
+		return
 	}
+	if s.cnt == 1 {
+		s.csync <- 1
+	} 
+	s.cnt--
+	s.syncmutex.Leave()
+}
+
+func (s *Sync)Wait() {
+	<- s.csync
 }
--- tests/sync/test_sync.go
+++ tests/sync/test_sync.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	sync "github.com/crmathieu/pkg/sync"
+	sync "github.com/crmathieu/gosem/pkg/sync"
 	"time"
+	"fmt"
 )
 
 func main() {
 	s := sync.NewSync(5)
 
 	for i:=0; i<5; i++ {
-		go func() {
-			time.Sleep(1 * time.Duration.Second)
-		}()
+		go func(a int) {
+			defer s.Done()
+			time.Sleep(time.Duration(a) * time.Second)
+			fmt.Println("Finishing", a)
+		}(i)
+		s.Add()
 	}
+	s.Wait()
+	fmt.Println("Main Finished...")
 }
